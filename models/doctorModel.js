8ccf8b4b6a17f16402c46330888f91978e24c801
Luis E. Julio Padilla <ljuliop@unicartagena.edu.co>
2024-09-25 17:24:05 -0500

    re organizacion de doctorModel

--- models/doctorModel.js
+++ models/doctorModel.js
@@ -1,38 +1,14 @@
 const mongoose = require("mongoose");
 const doctorSchema = new mongoose.Schema(
   {
-    userId: {
-      type:String,
-      required: true,
-    },
-    firstName: {
-      type: String,
-      required: true,
-    },
-    lastName: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-    },
-    phoneNumber: {
-      type: String,
-      required: true,
-    },
-    department: {
-      type: String,
-      required: true,
-    },
-   timings:{
-    type: Array,
-    required: true,
-   },
-   status:{
-    type: String,
-    default: "pending",
-   },
+    userId: {type:String,required: true,},
+    firstName: {type: String,required: true,},
+    lastName: {type: String,required: true,},
+    email: {type: String,required: true,},
+    phoneNumber: {type: String,required: true,},
+    department: {type: String,required: true,},
+   timings:{type: Array,required: true,},
+   status:{type: String,default: "pending",},
   },
   {
     timestamps: true,
