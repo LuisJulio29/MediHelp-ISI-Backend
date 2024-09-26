725b3129306f17ca96efb6332132241cb7a528c8
Luis E. Julio Padilla <ljuliop@unicartagena.edu.co>
2024-09-26 01:41:36 -0500

    DoctorFormComponent

--- routes/doctorRoute.js
+++ routes/doctorRoute.js
@@ -5,7 +5,7 @@ const authMiddlewares = require('../middlewares/authMiddlewares');
 
 router.post("/get-doctor-info-by-user-id",authMiddlewares, async (req,res) => {
     try {
-      const doctor = await Doctor.findOne({_id: req.body.userId});
+      const doctor = await Doctor.findOne({userId: req.body.userId});
       res
         .status(200)
         .send({
@@ -17,6 +17,26 @@ router.post("/get-doctor-info-by-user-id",authMiddlewares, async (req,res) => {
       res
         .status(500)
         .send({ message: "Error al obtener la informacion del Doctor", success: false, error });
+    }
+  });
+
+
+router.post("/update-doctor-profile",authMiddlewares, async (req,res) => {
+    try {
+      const doctor = await Doctor.findOneAndUpdate({userId: req.body.userId},
+        req.body,
+      );
+      res
+        .status(200)
+        .send({
+          message: "Doctor Actualizado Satistactoriamente",
+          success: true,
+          data: doctor,
+        }); 
+    } catch (error) {
+      res
+        .status(500)
+        .send({ message: "Error al actualizar la informacion del  Doctor", success: false, error });
     }
   });
 
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 require('dotenv').config();
 const dbconfig = require('./config/dbconfig');
 app.use(express.json());
+const path = require('path');
 const userRoute = require('./routes/userRoute');
 const adminRoute = require('./routes/adminRoute');
 const doctorRoute = require('./routes/doctorRoute');
@@ -14,12 +15,10 @@ app.use("/api/admin", adminRoute);
 app.use("/api/doctor", doctorRoute);
 const port = process.env.PORT || 5000;
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
