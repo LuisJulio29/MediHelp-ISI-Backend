329ac209452380c3d88c028fa402863992c3dd74
Luis E. Julio Padilla <ljuliop@unicartagena.edu.co>
2024-11-18 13:55:48 -0500

    add unique code and email

--- models/userModel.js
+++ models/userModel.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    code: { type: String, required: true },
-    email: { type: String, required: true },
+    code: { type: String, required: true, unique : true },
+    email: { type: String, required: true, unique : true },
     password: { type: String, required: true },
     isDoctor: { type: Boolean, default: false },
     isAdmin: { type: Boolean, default: false },
